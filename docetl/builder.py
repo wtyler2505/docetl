81451811223e7eb5926df4fe456b4747c71a5b7c
Egil <egil.moller@freecode.no>
2024-10-05 19:02:19 +0200

    Send around the Optimizer too, just like the DSLRunner

--- docetl/builder.py
+++ docetl/builder.py
@@ -88,7 +88,7 @@ class Optimizer(Pipeline):
 
         base_name = yaml_file.rsplit(".", 1)[0]
         suffix = yaml_file.split("/")[-1].split(".")[0]
-        return Pipeline.from_yaml(
+        return super(Optimizer, cls).from_yaml(
             yaml_file,
             base_name=base_name,
             yaml_file_suffix=suffix,
@@ -196,6 +196,7 @@ class Optimizer(Pipeline):
             try:
                 operation_class = get_operation(operation_type)
                 operation_class(
+                    self,
                     operation_config,
                     self.config.get("default_model", "gpt-4o-mini"),
                     self.max_threads,
@@ -976,6 +977,7 @@ class Optimizer(Pipeline):
                     f"Dataset '{dataset_name}' not found in config or previous steps."
                 )
             dataset = Dataset(
+                runner=self,
                 type=dataset_config["type"],
                 path_or_data=dataset_config["path"],
                 parsing=dataset_config.get("parsing", []),
@@ -1118,6 +1120,7 @@ class Optimizer(Pipeline):
             List[Dict[str, Any]]: The optimized operation configuration.
         """
         reduce_optimizer = ReduceOptimizer(
+            self, 
             self.config,
             self.console,
             self.llm_client,
@@ -1160,6 +1163,7 @@ class Optimizer(Pipeline):
         new_right_name = right_name
         for _ in range(max_iterations):
             join_optimizer = JoinOptimizer(
+                self, 
                 self.config,
                 op_config,
                 self.console,
@@ -1281,6 +1285,7 @@ class Optimizer(Pipeline):
             List[Dict[str, Any]]: The optimized operation configuration.
         """
         map_optimizer = MapOptimizer(
+            self, 
             self.config,
             self.console,
             self.llm_client,
@@ -1310,6 +1315,7 @@ class Optimizer(Pipeline):
             List[Dict[str, Any]]: The optimized operation configuration.
         """
         optimized_config, cost = JoinOptimizer(
+            self, 
             self.config,
             op_config,
             self.console,
@@ -1357,6 +1363,7 @@ class Optimizer(Pipeline):
         operation_class = get_operation(op_config["type"])
 
         oc_kwargs = {
+            "runner": self,
             "config": op_config,
             "default_model": self.config["default_model"],
             "max_threads": self.max_threads,
